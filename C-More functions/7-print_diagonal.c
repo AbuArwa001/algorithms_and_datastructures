a92adde4e0de34d5b28931c377ab5de751c2824a
AbuArwa001 <khalfanathman12@yahoo.com>
2024-10-08 08:21:31 +0300

    Refactor: Add _putchar function to C-More functions
    This commit adds the _putchar function to the C-More functions directory. The _putchar function writes a character to stdout. It takes a character as input and returns 1 on success. On error, it returns -1 and sets errno appropriately.

---
+++ C-More functions/7-print_diagonal.c
@@ -0,0 +1,15 @@
+#include "main.h"
+void print_diagonal(int n)
+{
+	int i = 0, j = 0;
+
+	for (i = 0; i < n; i++)
+	{
+		for (j = 0; j < i; j++)
+		{
+			_putchar(' ');
+		}
+		_putchar('\\');
+		_putchar('\n');
+	}
+}
