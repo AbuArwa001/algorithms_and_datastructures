4af5db68e0968f69f189f0a95b5c919d1a472e5e
AbuArwa001 <khalfanathman12@yahoo.com>
2024-10-26 10:09:54 +0300

    Refactor Pascal's Triangle generation code

--- Arrays/Basics_of_Arrays/python_solutions/118_pascal's_triangle.py
+++ Arrays/Basics_of_Arrays/python_solutions/118_pascal's_triangle.py
@@ -1,4 +1,18 @@
+from typing import List
+
+
 class Solution:
+    """
+    A class used to generate Pascal's Triangle.
+    Methods
+    -------
+    generate(numRows: int) -> List[List[int]]:
+        Generates the first `numRows` of Pascal's Triangle.
+        Parameters:
+        numRows (int): The number of rows of Pascal's Triangle to generate.
+        Returns:
+        List[List[int]]: A list of lists, where each inner list represents a row of Pascal's Triangle.
+    """
     def generate(self, numRows: int) -> List[List[int]]:
         triangle = []
         for i in range(numRows):
